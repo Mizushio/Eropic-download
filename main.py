4e57d484cc6ef792d1315845ea942e154da83a1d
水汐 <961060510@qq.com>
2022-07-05 11:47:39 +0800

    调整了一丢丢

--- main.py
+++ main.py
@@ -2,28 +2,20 @@ import urllib.request
 import urllib
 
 localDir = "./pic/"
-
-urlList = [
-"https://hentaipaw.com/img/1/80/806150/"
-]
 headers=('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36')
 opener = urllib.request.build_opener()#实例化一个OpenerDirector
 opener.addheaders = [headers]#添加header,注意格式
 urllib.request.install_opener(opener)#将OpenerDirector装进opener
-
-for i in range(159):
-    everyURL="https://hentaipaw.com/img/1/80/806150/"+str(i+1)+".webp"
+j=15
+for i in range(2000):
+    everyURL="https://hentaipaw.com/img/2/225/2254572/"+str(i+1)+".webp"
+    # 这边写自己找到的本本的网址+后续格式
+    if (j < 0):
+        print("over!")
+        break
     try:
-        urllib.request.urlretrieve(everyURL,localDir+str(i+1)+".webp")
+        urllib.request.urlretrieve(everyURL,localDir+str(i+1)+".jpg")
         print("download ok:"+everyURL)
     except:
         print("!!!download error:" + everyURL)
-
-    # for everyFile in urlList:
-#     everyURL = everyFile
-#     # url split
-#     localFileName = everyURL.split('/')[-1]
-#     localFile = localDir  +  localFileName
-# #     print (localFile)    # test strings
-#     urllib.request.urlretrieve(everyURL, localFile)
-#     print("download ok:"+localFileName)
+        j=j-1
